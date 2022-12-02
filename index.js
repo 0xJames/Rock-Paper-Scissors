edb5de3e4c7317011413833cca8ab32c111f7531
James Yang <jy0xcs@gmail.com>
2022-12-01 22:05:20 -0800

    Finish Rock-paper-Scissor exercise.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log("Hello World!")
+function getComputerChoice(){
+    let randomNumber = Math.random()
+    console.log(randomNumber)
+    if (randomNumber < 1/3) {
+        return "Rock"}
+    else if (randomNumber < 2/3) {
+        return "Paper"}
+    else {
+        return "Scissor"}
+
+    }
+ // randomly return 1 of 3 choices
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection == "Rock" && computerSelection == "Scissor"){
+        return "You Win! Rock beats Scissor"
+    } 
+    else if (playerSelection == "Rock" && computerSelection == "Rock"){
+        return "Tie!"
+    } 
+    else if (playerSelection == "Rock" && computerSelection == "Paper"){
+        return "LOSER!"
+    } 
+}
+
+let playerSelection = prompt("Make your selection!");
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection,computerSelection));
+
+function game(){
+    for (let i =0; i<5;i++){
+        playRound(playerSelection, computerSelection)
+        console.log(game())
+    }
+}
